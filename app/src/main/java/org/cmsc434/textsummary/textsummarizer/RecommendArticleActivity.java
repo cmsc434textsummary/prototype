902192c335790826452dbe3bfea480f72e0862c4
Hojoon Lee <joon21@terpmail.umd.edu>
2017-11-04 17:59:34 -0400

    initial commit

---
+++ app/src/main/java/org/cmsc434/textsummary/textsummarizer/RecommendArticleActivity.java
@@ -0,0 +1,15 @@
+package org.cmsc434.textsummary.textsummarizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecommendArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend_article);
+
+        /*   code here   */
+    }
+}
---
+++ app/src/main/java/org/cmsc434/textsummary/textsummarizer/SummaryConfigActivity.java
@@ -0,0 +1,16 @@
+package org.cmsc434.textsummary.textsummarizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SummaryConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary_config);
+
+        /*  code here  */
+
+    }
+}
