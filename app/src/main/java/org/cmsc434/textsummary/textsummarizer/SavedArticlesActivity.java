ba399784922a38139e33bc98c96a4760a4887b49
biqua <emilyswinner@gmail.com>
2017-11-05 11:00:40 -0500

    --Added Login page, Settings/Menu page, and Saved Articles page                                             --Main menu buttons have basic functionality

---
+++ app/src/main/java/org/cmsc434/textsummary/textsummarizer/SavedArticlesActivity.java
@@ -0,0 +1,17 @@
+package org.cmsc434.textsummary.textsummarizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedArticlesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_articles);
+
+        /* The layout for this page should be basically the same as
+        // the recommended articles activity
+         */
+    }
+}
